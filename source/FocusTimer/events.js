a815422bce1581ba0dfa524f3163788989816618
Arthur Erpen <77343288+artherp@users.noreply.github.com>
2024-08-09 18:52:48 -0300

    dataset para controle das ações

---
+++ source/FocusTimer/events.js
@@ -0,0 +1,10 @@
+import { controls } from "./elements.js";
+
+export function registerControls() {
+    controls.addEventListener('click', (event) => {
+       const action = console.log(event.target.dataset.action)
+       if(action === undefined) {
+        return
+       }
+    })
+}
