e0d426a8ccfcd2ecf26e083e1991d692167eb96f
Arthur Erpen <77343288+artherp@users.noreply.github.com>
2024-08-12 16:07:45 -0300

    conectando as primeiras ações

--- source/FocusTimer/events.js
+++ source/FocusTimer/events.js
@@ -1,10 +1,14 @@
 import { controls } from "./elements.js";
+import * as actions from './actions.js'
 
 export function registerControls() {
     controls.addEventListener('click', (event) => {
        const action = console.log(event.target.dataset.action)
-       if(action === undefined) {
+       if(typeof actions[action] != "function") {
         return
        }
+
+
+       actions[action]()
     })
 }
